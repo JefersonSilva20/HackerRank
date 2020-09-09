6724150ac0fee4ddfbd54089d3169896f0bbeb19
Jeferson Silva <jefersoun.silva@gmail.com>
2020-09-09 19:42:36 -0300

    plusMinus

---
+++ PlusMinus/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+        int positiveNumber = 0, negativeNumber = 0, neutroNumber = 0;
+        for (int i : arr) {
+            if (i > 0) {
+                positiveNumber++;
+            } else if (i < 0) {
+                negativeNumber++;
+            } else {
+                neutroNumber++;
+            }
+        }
+        System.out.println((double)positiveNumber/arr.length);
+        System.out.println((double)negativeNumber / arr.length);
+        System.out.println((double)neutroNumber / arr.length);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        plusMinus(new int[] { -4, 3, -9, 0, 4, 1 });
+    }
+}
