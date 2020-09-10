52b77416c7484ec5cde4ce8ca1cb37f7137ecb5f
Jeferson Silva <jefersoun.silva@gmail.com>
2020-09-09 21:04:52 -0300

    StairCase

---
+++ StairCase.java
@@ -0,0 +1,22 @@
+public class StairCase {
+
+    static void staircase(int n) {
+        String p = "";
+        for(int i = 0 ; i< n ; i++){
+            p = p + "#";
+            System.out.printf("%"+n+"s\n", p);
+           
+        }
+        //Java 11
+        /*for (int i = 0; i <= n; i++) {
+            System.out.print(" ".repeat(n - i));
+            System.out.print("#".repeat(i));
+            System.out.println("");
+        }*/
+
+    }
+
+    public static void main(String[] args) {
+        staircase(6);
+    }
+}
