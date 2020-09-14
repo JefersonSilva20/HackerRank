67e54438b38befba89c03c7bc38d3fa047ca3fe7
Jeferson Silva <jefersoun.silva@gmail.com>
2020-09-13 21:14:18 -0300

    Apple and Orange

---
+++ appleandorange/AppleAndOrange.java
@@ -0,0 +1,38 @@
+package appleandorange;
+
+public class AppleAndOrange {
+
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int countApples = calDistances(apples, a, s, t);
+        int countOranges = calDistances(oranges, b, s, t);
+        System.out.println(countApples);
+        System.out.println(countOranges);
+
+    }
+
+    /*
+     * static int countFruits(int[] fruitPositionsTree, int samsHouseStarting, int
+     * samHouseEnding) { int countFruit = 0; for (int i = 0; i <
+     * fruitPositionsTree.length; i++) { if (fruitPositionsTree[i] >=
+     * samsHouseStarting && fruitPositionsTree[i] <= samHouseEnding) { countFruit++;
+     * } } return countFruit; }
+     */
+
+    static int calDistances(int[] fruitsPositions, int localizationTree, int samsHouseStarting, int samHouseEnding) {
+        int fruitPositionsTree = 0;
+        int countFruit = 0;
+        for (int i = 0; i < fruitsPositions.length; i++) {
+            fruitPositionsTree = fruitsPositions[i] + localizationTree;
+            if (fruitPositionsTree >= samsHouseStarting && fruitPositionsTree <= samHouseEnding) {
+                countFruit++;
+            }
+
+        }
+        return countFruit;
+    }
+
+    public static void main(String[] args) {
+        countApplesAndOranges(7, 11,5, 15,new int[]{-2, 2, 1},new int[]{5,-6});
+
+    }
+}
