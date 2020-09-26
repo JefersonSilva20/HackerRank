11cd69ff40383ebff31226aaf8be6331a698f5f6
Jeferson Silva <jefersoun.silva@gmail.com>
2020-09-25 21:14:48 -0300

    divisible-sum-pair

--- divisiblesumpair/DivisibleSumPair.java
+++ divisiblesumpair/DivisibleSumPair.java
@@ -6,9 +6,9 @@ public class DivisibleSumPair {
     static int divisibleSumPairs(int n, int k, int[] ar) {
         int count = 0;
         for (int i = 0; i < ar.length; i++) {
-            for (int j = i+1; j < ar.length; j++) {
+            for (int j = i + 1; j < ar.length; j++) {
                 if (((ar[i] + ar[j]) % k) == 0) {
-                    System.out.println(ar[i]+" + "+ar[j]);
+                    System.out.println(ar[i] + " + " + ar[j]);
                     count++;
                 }
             }
